78a7b8c2d0a78134a2227159fa95990081929d53
irenealcaine <101673351+irenealcaine@users.noreply.github.com>
2025-08-29 16:11:54 +0200

    Initial commit

---
+++ src/Components/Loader/Loader.jsx
@@ -0,0 +1,7 @@
+import "./Loader.css";
+
+const Loader = ({ color }) => {
+  return <span className={`loader ${color}`}></span>;
+};
+
+export default Loader;
---
+++ src/Pages/Page4/Page4.jsx
@@ -0,0 +1,11 @@
+import "./Page4.css";
+
+const Page4 = () => {
+  return (
+    <div className="page4">
+      <h1>Page 4</h1>
+    </div>
+  );
+};
+
+export default Page4;
