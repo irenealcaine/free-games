1023a6da4526d68bc79ffeddec54a2e40e74527a
irenealcaine <101673351+irenealcaine@users.noreply.github.com>
2025-09-03 16:01:51 +0000

    upgrade footer

--- src/Utils/Constants.jsx
+++ src/Utils/Constants.jsx
@@ -1,3 +1,7 @@
+import { AiOutlineLinkedin } from "react-icons/ai";
+import { LuGithub, LuLinkedin  } from "react-icons/lu";
+import { TbWorldWww } from "react-icons/tb";
+
 export const navbarItems = [
   {
     "name": "Free games",
@@ -5,5 +9,21 @@ export const navbarItems = [
   }, {
     "name": "Giveaways",
     "slug": "giveaways"
+  },
+]
+
+export const socialMediaItems = [
+  {
+    "name": "LinkedIn",
+    "icon": <LuLinkedin />,
+    "url":"https://www.linkedin.es"
+  }, {
+    "name": "GitHub",
+    "icon": <LuGithub />,
+    "url":"https://www.github.es"
+  },{
+    "name": "Web",
+    "icon": <TbWorldWww />,
+    "url":"https://www.irenealcainealvarez.es"
   },
 ]
