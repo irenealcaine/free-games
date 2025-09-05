d9163306a60da638090db13c5063a6e7f5d23dac
irenealcaine <101673351+irenealcaine@users.noreply.github.com>
2025-09-05 08:36:35 +0000

    game details

--- src/Pages/GameDetails/GameDetails.jsx
+++ src/Pages/GameDetails/GameDetails.jsx
@@ -1,6 +1,7 @@
 import { Link, useParams, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import "./GameDetails.css"
 
 
 const GameDetailsPage = () => {
@@ -28,36 +29,46 @@ const GameDetailsPage = () => {
   }, []);
 
   return (
-    <div>
-      <h1>{game.title}</h1>
-      <p>{game.description}</p>
-      <img src={game.thumbnail} />
-      <p>{game.developer}</p>
-      <p>{game.publisher}</p>
-      <p>{game.genre}</p>
-      <p>{game.platform}</p>
-      <p>{game.release_date}</p>
+    <div className="game-details">
+      <div className="game-head">
+        <img src={game.thumbnail} />
+        <div className="game-info">
+          <h1>{game.title}</h1>
+          <p>Developer: {game.developer}</p>
+          <p>Pueblisher: {game.publisher}</p>
+          <p>Release date: {game.release_date}</p>
+          <div>
+            <p>{game.genre}</p>
+            <p>{game.platform}</p>
+          </div>
+        </div>
+      </div>
+      <p className="game-description">{game.description}</p>
       <p>{game.status}</p>
       {game.minimum_system_requirements
-        ? <div>
-          <p>minimum system requirements</p>
+        ? <div className="game-requirements">
+          <h2>Minimum system requirements</h2>
+          <div className="requirements">
           {Object.entries(game.minimum_system_requirements).map(([key, value]) =>
             value ? (
-              <p key={key}>
+              <p className="requirement" key={key}>
                 {key}: <span>{value}</span>
               </p>
             ) : null
           )}
+          </div>
         </div>
         : ""}
 
-      {game.screenshots ? <div>
+
+      {game.screenshots ? <div className="game-images">
         {game.screenshots.map((img) => (
           <img src={img.image} />
         ))}
-      </div> : ""}
-      {console.log(game)}
-    </div>
+      </div> : null}
+        </div>
+
+
   );
 };
 
